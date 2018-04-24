53519ccd78f64dd052c98fc3e565db76a46eb240
Alvaro <farinalvaro@gmail.com>
2018-04-24 00:53:39 -0300

    Usuario y token listo

---
+++ routes/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+app = express();
+
+// Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+});
+
+module.exports = app;
---
+++ routes/login.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+app = express();
+
+var Usuario = require('../models/usuario');
+
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    Usuario.findOne({email: body.email}, (err, usuarioDB) => {
+
+        if(err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        // para prod sacar el campo que fallo, esto es para dev
+        if(!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - email',
+                errors: err
+            });
+        }
+
+        if( !bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - pass',
+                errors: err
+            });
+        }
+
+        // para no mandar la contrasena en el token
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+
+        return res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+            token: token,
+            id: usuarioDB._id
+        });
+    });
+
+});
+
+
+module.exports = app;
