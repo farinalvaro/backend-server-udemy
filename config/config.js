8af4a0e6e80847dcbfce837776c7d79a76f653c7
Alvaro <farinalvaro@gmail.com>
2018-05-30 00:05:55 -0300

    Seccion 12 terminada - backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// Google
+module.exports.CLIENT_ID = '120830474931-1vu3l5j28bb73mvgl5av4jqob1pnupof.apps.googleusercontent.com';
+
--- models/usuario.js
+++ models/usuario.js
@@ -16,7 +16,8 @@ var usuarioSchema = new Schema({
     email: { type: String, unique: true, required: [true, 'El correo es requerido'] },
     password: { type: String, required: [true, 'El pass es requerido'] },
     img: { type: String, required: false },
-    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+    google: { type: Boolean, default: false }
 });
 
 usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser unico'});
